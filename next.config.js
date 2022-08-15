9a1b544c7b77149468088715da3c24522431ea8c
Lowe Fredriksson Eklund <lowe@storesprint.app>
2022-08-15 23:04:05 +0200

    controls

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
   webpack(config, options) {
     config.module.rules.push({
       test: /\.mp3$/,
